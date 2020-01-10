e9d5a64a0924e424e1081274a19f50d376d500c8
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2020-01-10 13:45:35 +0100

    Init commit

---
+++ src/main/java/rs/ac/ftn/uns/upp/scintificcenter/ScintificcenterApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.uns.upp.scintificcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScintificcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScintificcenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/ftn/uns/upp/scintificcenter/ScintificcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.uns.upp.scintificcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScintificcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
