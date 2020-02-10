ac4e2bb35e2559b966bf900424e796140b14dd31
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2020-02-10 12:41:46 +0100

    Persist article bug fix

--- src/main/java/rs/ac/ftn/uns/upp/scientificcenter/service/task/PersistArticleService.java
+++ src/main/java/rs/ac/ftn/uns/upp/scientificcenter/service/task/PersistArticleService.java
@@ -7,15 +7,23 @@ import rs.ac.ftn.uns.upp.scientificcenter.bom.Article;
 import rs.ac.ftn.uns.upp.scientificcenter.service.ArticleService;
 import rs.ac.ftn.uns.upp.scientificcenter.utils.MapperUtils;
 
+import static java.util.Objects.isNull;
+
 public class PersistArticleService implements JavaDelegate {
     @Autowired
     private ArticleService articleService;
 
     @Override
     public void execute(DelegateExecution execution) {
-        Article article = MapperUtils.map(execution.getVariables(), Article.class);
-        article.setAbstractText((String) execution.getVariable("abstract"));
-        article.setActive(false);
+        Article article = articleService.findByTitle((String) execution.getVariable("title"));
+
+        if (isNull(article)) {
+            article = MapperUtils.map(execution.getVariables(), Article.class);
+            article.setAbstractText((String) execution.getVariable("abstract"));
+            article.setActive(false);
+        } else {
+            article.setPdf((String) execution.getVariable("pdf"));
+        }
 
         articleService.save(article);
     }
